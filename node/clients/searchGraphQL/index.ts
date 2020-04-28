06e2332840e9d8140ffe45d8ffc4d0f84c4c2741
viniagostini <viniciusaagostini@gmail.com>
2020-04-28 15:47:35 -0300

    stop calling search-graphql directly

--- node/clients/searchGraphQL/index.ts
+++ node/clients/searchGraphQL/index.ts
@@ -1,5 +1,5 @@
+import { GraphQLServer } from './../graphqlServer';
 import {
-  AppGraphQLClient,
   InstanceOptions,
   IOContext,
   Serializable,
@@ -7,26 +7,25 @@ import {
 
 import {
   ProductArgs,
-  ProductResponse,
   query as productQuery,
+  ProductResponse,
 } from './productQuery'
 
-export class SearchGraphQL extends AppGraphQLClient {
+const extensions = {
+  persistedQuery: {
+    provider: 'vtex.search-graphql@0.x',
+    sender: 'vtex.checkout-graphql@0.x',
+  },
+}
+
+export class SearchGraphQL extends GraphQLServer {
   constructor(ctx: IOContext, opts?: InstanceOptions) {
-    super('vtex.search-graphql@0', ctx, opts)
+    super(ctx, opts)
   }
 
   public product = <T extends Serializable = ProductResponse>(
     variables: ProductArgs,
     query: string = productQuery
   ) =>
-    this.graphql.query<T, ProductArgs>(
-      {
-        query,
-        variables,
-      },
-      {
-        metric: 'get-product',
-      }
-    )
+    this.query<T>(query, variables, extensions, { metric: 'get-product' })
 }
