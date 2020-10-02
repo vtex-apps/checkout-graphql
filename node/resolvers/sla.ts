0a68531dfbfbfd668d3b4977f9232b9df8186996
Luciano Júnior <luciannojunior@gmail.com>
2020-10-02 12:45:25 -0300

    Using the same util function now mocking the OF

--- node/resolvers/sla.ts
+++ node/resolvers/sla.ts
@@ -1,11 +1,20 @@
-import {  getShippingInfoForSimulation } from "../utils/shipping"
+import {  getShippingInfo } from "../utils/shipping"
 
 export const queries = {
   shippingSLA: async (_: any, args: any, ctx: Context) => {
     const checkout = ctx.clients.checkout
-    const simulationData = await checkout.simulation(args) as CheckoutOrderForm['shippingData']
+    const shippingData = await checkout.simulation(args) as CheckoutOrderForm['shippingData']
 
-    console.log({ simulationData })
-    return getShippingInfoForSimulation(simulationData)
+    // Here we mock an Order Form object to use getShippingInfo
+    const orderForm = {
+      orderFormId: '',
+      shippingData: {
+        ...shippingData,
+        selectedAddresses: []
+      },
+      totalizers: []
+    } as unknown as CheckoutOrderForm
+
+    return await getShippingInfo({ clients: ctx.clients, orderForm})
   },
 }
