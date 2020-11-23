b08bd1055504728c218073345ad7f9bbe53a5cd9
Jefferson <jefiim.ad@gmail.com>
2020-11-23 17:00:21 -0300

    Format pickup point's business hours

---
+++ node/utils/pickup.ts
@@ -0,0 +1,87 @@
+const DAYS_ORDER = [1, 2, 3, 4, 5, 6, 0]
+const MONDAY_INDEX = 0
+const FRIDAY_INDEX = 4
+
+const normalizeBusinessHours = (businessHours: BusinessHour[]) => {
+  return DAYS_ORDER.map(dayNumber => {
+    const day = businessHours.find(bh => dayNumber === bh.DayOfWeek)
+
+    const dayObject = day && {
+      closed: false,
+      openingTime: day.OpeningTime,
+      closingTime: day.ClosingTime,
+    }
+
+    return {
+      dayNumber,
+      closed: true,
+      openingTime: '',
+      closingTime: '',
+      ...(dayObject ?? {}),
+    }
+  })
+}
+
+const doesWeekDaysHaveTheSameHours = (
+  businessHours: NormalizedBusinessHour[]
+) => {
+  const weekDays = businessHours.slice(MONDAY_INDEX, FRIDAY_INDEX)
+
+  let previousOpeningTime = weekDays[0].openingTime
+  let previousClosingTime = weekDays[0].closingTime
+
+  for (let i = 1; i < weekDays.length; i++) {
+    const currentDay = weekDays[i]
+
+    if (
+      currentDay.openingTime !== previousOpeningTime ||
+      currentDay.closingTime !== previousClosingTime
+    ) {
+      return false
+    }
+
+    previousOpeningTime = currentDay.openingTime
+    previousClosingTime = currentDay.closingTime
+  }
+
+  return true
+}
+
+const condenseWeekDaysHours = (businessHours: NormalizedBusinessHour[]) => {
+  return businessHours.reduce(
+    (
+      acc: NormalizedBusinessHour[],
+      businessHour: NormalizedBusinessHour,
+      index: number
+    ) => {
+      if (index >= MONDAY_INDEX && index <= FRIDAY_INDEX) {
+        if (index === MONDAY_INDEX) {
+          return acc.concat({
+            dayNumber: '1to5',
+            closed: businessHour.closed,
+            openingTime: businessHour.openingTime,
+            closingTime: businessHour.closingTime,
+          })
+        }
+        return acc
+      }
+
+      return acc.concat(businessHour)
+    },
+    []
+  )
+}
+
+export function formatBusinessHoursList(businessHours: BusinessHour[]) {
+  const normalizedBusinessHours = normalizeBusinessHours(businessHours)
+
+  const weekDaysHaveTheSameHours = doesWeekDaysHaveTheSameHours(
+    normalizedBusinessHours
+  )
+
+  if (weekDaysHaveTheSameHours) {
+    return condenseWeekDaysHours(normalizedBusinessHours)
+  }
+
+  return normalizedBusinessHours
+}
