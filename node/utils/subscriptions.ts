e194fe1eaff77bee7a87f0edd4cf06ce7a347894
victorhmp <victor2142@gmail.com>
2021-03-26 16:06:35 -0300

    Add subcriptions utils

---
+++ node/utils/subscriptions.ts
@@ -0,0 +1,57 @@
+const FREQUENCY_PATTERN = /(\d{1,3})?\s*(\w*?)(?:$|s|ly)/i
+
+const SUBSCRIPTION_PREFIX = `vtex.subscription`
+const SUBSCRIPTION_KEY_PREFIX = `${SUBSCRIPTION_PREFIX}.key`
+
+const SUBSCRIPTION_KEY_FREQUENCY = `${SUBSCRIPTION_KEY_PREFIX}.frequency`
+
+export function parseFrequency(frequency?: string) {
+  if (frequency == null) return
+
+  const match = frequency.match(FREQUENCY_PATTERN)
+
+  if (!match) {
+    return
+  }
+
+  const [, count = '1', type] = match
+
+  if (!type) return
+
+  return {
+    interval: +count,
+    periodicity: type.toUpperCase(),
+  } as SubscriptionDataEntry['plan']['frequency']
+}
+
+export function generateSubscriptionDataEntry(
+  subscriptionsFromAssemblyOptions: Array<{
+    itemIndex: number
+    options: AssemblyOptionInput[]
+  }>
+): SubscriptionDataEntry[] {
+  const planType = 'RECURRING_PAYMENT'
+  const defaultExecutionCount = 0
+
+  const subscriptionDataEntries = subscriptionsFromAssemblyOptions.map(item => {
+    const { itemIndex } = item
+
+    const planFrequency = parseFrequency(
+      item.options?.[0].inputValues[SUBSCRIPTION_KEY_FREQUENCY]
+    )
+
+    const subscriptionPlan = {
+      frequency: planFrequency!,
+      type: planType,
+      validity: {},
+    }
+
+    return {
+      executionCount: defaultExecutionCount,
+      itemIndex,
+      plan: subscriptionPlan,
+    }
+  })
+
+  return subscriptionDataEntries
+}
